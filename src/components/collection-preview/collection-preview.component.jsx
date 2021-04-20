1530b351fea9de351f940a9d99c912e8ea46a919
Amin Aghaee <amin_wdman@yahoo.com>
2021-04-20 12:25:49 -0400

    update app with shopPage ,CollectionPreview and CollectioItem components

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CollectionItem from '../collection-item/collection-item.component'
+import './collection-preview.styles.scss'
+
+const CollectionPreview = ({title , items}) => {
+    return (
+        <div className='collection-preview'>
+            <h1>{title.toUpperCase()}</h1>
+            <div className='preview'>
+                {items.filter((item,index)=>{return index<4}).map(({id, ...rest})=>{
+                    return <CollectionItem key={id} {...rest} />})}
+            </div>
+        </div>
+    )
+}
+
+export default CollectionPreview
